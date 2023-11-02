513e057ee9a2494699a2eb4e62eade48faf86e2e
Elle O'Brien <elleobrien11@gmail.com>
2023-11-02 13:52:04 -0400

    script gives you a compliment now

--- count_letters.py
+++ count_letters.py
@@ -5,6 +5,7 @@ def count_letters(input_string):
     for char in input_string:
         if char.isalpha():
             count += 1
+    print("You look amazing today!")
     return count
 
 if __name__ == '__main__':
